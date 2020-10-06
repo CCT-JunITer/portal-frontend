47fd6d101aa6630aef50250a07251069d5b2949e
Lukas Hoffmann <lukas.hoffmann@cct-ev.de>
2020-10-06 22:50:23 +0000

    Add CCT Colors to main theme and change color of headline

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,10 +1,22 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify';
+import { colors } from 'vuetify/lib';
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
   icons: {
     iconfont: 'mdi',
+  },
+  theme:{
+    themes: {
+      light: {
+        cctBlue: '#1e467d',
+        cctOrange: 'FF6400',
+        cctGreen: '#3E8A83',
+        cctLightGrey:'a5a5a5',
+        cctGrey:'646464',
+      },
+    },
   },
 });
