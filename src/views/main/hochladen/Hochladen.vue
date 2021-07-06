e4b41519894899fa80840a42e8d2707bf018a80e
vladyslav.lemberg <vladyslav.lemberg@cct-ev.de>
2021-07-06 16:17:56 +0000

    Navigation drawer

---
+++ src/views/main/hochladen/Hochladen.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="root">
+    <iframe src="https://cloud.cct-ev.de/index.php/s/K4xoQcAqKpaQ4GA" width="50%" height="450">
+      
+    </iframe>
+    
+    <iframe src="https://cloud.cct-ev.de/index.php/s/6iHKJAzWwpxY5dW" width="50%" height="450">
+
+    </iframe>
+    
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+
+@Component({})
+export default class Hochladen extends Vue {
+
+}
+</script>
+
+<style lang="sass">
+@import '~vuetify/src/styles/styles.sass'
+
+@media #{map-get($display-breakpoints, 'md-and-up')}
+  .input-lg
+    max-width: 340px!important
+
+
+</style>
