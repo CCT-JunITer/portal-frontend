2e3d423f718a03964fc9fc991607b40166fc3350
User <vlad.jkr@gmail.com>
2021-07-28 20:15:01 +0200

    updated cloud links

--- src/views/main/hochladen/Hochladen.vue
+++ src/views/main/hochladen/Hochladen.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="root">
 
-    <iframe src="https://cloud.cct-ev.de/index.php/s/K4xoQcAqKpaQ4GA" width="50%" height="450" frameBorder="0"></iframe>
+    <iframe src="https://cloud.cct-ev.de/s/CyipqHnyizb3WeE" width="50%" height="450" frameBorder="0"></iframe>
     <v-divider
       vertical
     ></v-divider>
-    <iframe src="https://cloud.cct-ev.de/index.php/s/6iHKJAzWwpxY5dW" width="50%" height="450" frameBorder="0"></iframe>
+    <iframe src="https://cloud.cct-ev.de/s/r8J5p43M9wj3W34" width="50%" height="450" frameBorder="0"></iframe>
 
     
   </div>
