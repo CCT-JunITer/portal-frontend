2d82c288a29dbceb5cf9d1cbf39386a93a5e588d
Thomas Janke <thomas.janke@cct-ev.de>
2022-01-31 20:18:03 +0000

    Fix tests

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,7 @@ module.exports = {
   preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
   setupFiles: ['./tests/setup.ts'],
   transformIgnorePatterns: ['/node_modules/(?!vuetify)'],
-
+  moduleNameMapper: {
+    '\\.(css|less|sass|scss)$': '<rootDir>/tests/__mocks__/styleMock.js',
+  }
 }
