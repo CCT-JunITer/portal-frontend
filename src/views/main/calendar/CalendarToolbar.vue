6dcc88c1f017b0a632fe9fa0b0312348e06c9c30
Tobias Loch <Tobias.Loch@mail.de>
2022-05-12 19:37:44 +0200

    fixed some visual stuff

--- src/views/main/calendar/CalendarToolbar.vue
+++ src/views/main/calendar/CalendarToolbar.vue
@@ -1,59 +1,73 @@
 <template>
   <div class="toolbarContainer">
     <v-checkbox 
+      style="display:block;width:100%"
+      class="text-truncate text-no-wrap"
       v-model="calendar.active"
       :color="calendar.color"
-      ripple
-      :label="calendar.name"
       @change="clicked"
+      ripple
     >
-    </v-checkbox>
-    
-    <div style="width:100%"></div>
-    <v-menu offset-x>
-      <template v-slot:activator="{attrs, on }">
-        <v-btn 
-          icon
-          v-bind="attrs"
-          v-on="on"
-        >
-          <v-icon>mdi-dots-vertical</v-icon>
-        </v-btn>
+      <template v-slot:label>
+        <v-tooltip bottom>
+          <template v-slot:activator="{ on }">
+            <div
+              v-on="on"
+            >
+              {{calendar.name}}
+            </div>
+          </template>
+          {{calendar.name}}
+        </v-tooltip>
       </template>
+    </v-checkbox>
 
-      <v-list>
-        <v-list-item
-          link
-          @click="editName"
-        >
-          <v-list-item-icon>
-            <v-icon>mdi-pencil</v-icon>
-          </v-list-item-icon>
-          <v-list-item-title>Name bearbeiten</v-list-item-title>
-        </v-list-item>
+    <div style="width:100%; flex-shrink:100"></div>
+    <div style="justify-self:flex-end;">
+      <v-menu offset-x >
+        <template v-slot:activator="{attrs, on }">
+          <v-btn 
+            icon
+            v-bind="attrs"
+            v-on="on"
+          >
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </template>
 
-        <v-list-item
-          link
-          @click="editColor"
-        >
-          <v-list-item-icon>
-            <v-icon>mdi-pencil</v-icon>
-          </v-list-item-icon>
-          <v-list-item-title>Farbe bearbeiten</v-list-item-title>
-        </v-list-item>
+        <v-list>
+          <v-list-item
+            link
+            @click="editName"
+          >
+            <v-list-item-icon>
+              <v-icon>mdi-pencil</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Name bearbeiten</v-list-item-title>
+          </v-list-item>
 
-        <v-list-item
-          link
-          @click="deleteCalendar"
-        >
-          <v-list-item-icon>
-            <v-icon>mdi-delete</v-icon>
-          </v-list-item-icon>
-          <v-list-item-title>Löschen</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-menu>
+          <v-list-item
+            link
+            @click="editColor"
+          >
+            <v-list-item-icon>
+              <v-icon>mdi-pencil</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Farbe bearbeiten</v-list-item-title>
+          </v-list-item>
 
+          <v-list-item
+            link
+            @click="deleteCalendar"
+          >
+            <v-list-item-icon>
+              <v-icon>mdi-delete</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Löschen</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </div>
     <!-- <v-overlay
       :value="editable"
       opacity="1"
@@ -118,7 +132,7 @@ export default {
 .toolbarContainer {
   width:100%;
   display:flex;
-  justify-content: center;
+  justify-content: flex-start;
   align-items:center;
 }
 
